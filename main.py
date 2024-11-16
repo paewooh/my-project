5b4e6a255d7f681b3373e2d93895d527b168e456
Никита Соломатин <paewooh@gmail.com>
2024-11-16 18:26:36 +0300

    Added new line to main.py from dev3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello, World!")
-    print("This is an additional line from person2.")
+    print("This is an additional line from person3.")
 
 if __name__ == "__main__":
     main()
